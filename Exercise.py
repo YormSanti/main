7a00843d3c124f97572d7215108fdd8d58056013
unknown <yormsanti38@gimail.com>
2025-02-11 11:25:01 +0700

    first commit

---
+++ Exercise.py
@@ -0,0 +1,11 @@
+total = 0.0
+
+item1 =float(input("Enter price: ")) 
+item2 = float(input("Enter price: ")) 
+item3 = float(input("Enter price: ")) 
+
+total += item1
+total += item2
+total += item3
+
+print(total)
